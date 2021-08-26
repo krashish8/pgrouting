84eb65422c866c8de3f6e59598fe1b77cee7e0fd
Ashish Kumar <ashishkr23438@gmail.com>
2021-08-26 10:06:48 +0530

    strings.h

--- src/common/postgres_connection.c
+++ src/common/postgres_connection.c
@@ -27,6 +27,9 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #ifdef HAVE_STRING_H
 #  include <string.h>
 #endif
+#if defined(HAVE_STRINGS_H)
+#  include <strings.h>
+#endif
 #include "utils/builtins.h"
 
 #include "catalog/pg_type.h"
